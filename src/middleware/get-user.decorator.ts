1a6d50e3846cb64c5d5f2ab8b7e47d90e43ebb57
davidsonjose <91620722+Davidsonjose@users.noreply.github.com>
2023-09-11 00:11:38 +0100

    restructured user data

--- src/middleware/get-user.decorator.ts
+++ src/middleware/get-user.decorator.ts
@@ -3,5 +3,6 @@ import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 export const GetUser = createParamDecorator((_data, ctx: ExecutionContext) => {
   const req = ctx.switchToHttp().getRequest();
 
-  return req.user;
+  const { location, settings, pwd, ...others } = req.user;
+  return others;
 });
