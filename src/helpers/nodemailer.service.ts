b065fd2c05353c3441b29961057e66ba02a5299b
davidsonjose <91620722+Davidsonjose@users.noreply.github.com>
2023-10-26 15:21:13 +0100

    [update]-changed nodemailer

--- src/helpers/nodemailer.service.ts
+++ src/helpers/nodemailer.service.ts
@@ -1,6 +1,5 @@
 // otp/nodemailer.service.ts
-import { BadRequestException, Injectable } from '@nestjs/common';
-import axios from 'axios';
+import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 
 @Injectable()
@@ -9,47 +8,38 @@ export class NodemailerService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: 'smtp-relay.brevo.com', // Brevo SMTP server
-      port: 587, // Port 587 for TLS
-      secure: false,
+      host: 'lfzaccess.com', // Replace with your SMTP host
+      port: 465, // Use port 587 for TLS
+      secure: true, // Use TLS (false for port 587)
       auth: {
-        user: 'testbalosh@gmail.com', // Your email address
-        pass: 'DnCYMrjaSBU7z9pd', // Your master password
+        user: 'info@lfzaccess.com', // Gmail or cPanel email
+        pass: '0fw=[mW$RRP6', // Gmail or cPanel password
       },
     });
+    // this.transporter = nodemailer.createTransport({
+    //   host: 'smtp.lfzaccess.com', // Replace with your SMTP host
+    //   port: 587, // Use port 587 for TLS
+    //   secure: false, // Use TLS (false for port 587)
+    //   auth: {
+    //     user: 'info@lfzaccess.com', // Gmail or cPanel email
+    //     pass: '0fw=[mW$RRP6', // Gmail or cPanel password
+    //   },
+    // });
   }
 
   async sendMail(email: string, template: string, otp?: string): Promise<void> {
     try {
-      // const mailOptions = {
-      //   from: 'quixcelsolution@gmail.com', // Your email address
-      //   to: email,
-      //   subject: 'OTP Verification',
-      //   html: template,
-      // };
-      // await this.transporter.sendMail(mailOptions);
-
-      await axios({
-        method: 'post',
-        url: 'https://api.brevo.com/v3/smtp/email',
-        headers: {
-          'Content-Type': 'application/json',
-          'api-key':
-            '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',
-        },
-        data: {
-          sender: {
-            name: 'Chevyview Estate',
-            email: 'info@lfzaccess.com',
-          },
-          to: [{ email: email }],
-          subject: 'Otp Verification',
-          template,
-        },
-      });
+      const mailOptions = {
+        from: 'info@lfzaccess.com', // Change this to your email
+        to: email,
+        subject: 'OTP Verification',
+        // text: `Your OTP: ${otp}`,
+        html: template,
+      };
+      await this.transporter.sendMail(mailOptions);
     } catch (error) {
       console.error('Error sending email:', error);
-      throw new BadRequestException('Could not send email');
+      throw new Error('Could not send email');
     }
   }
 }
