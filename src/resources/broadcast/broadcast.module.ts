939ffc3da238e50066b18777eb90571e323d3daf
davidsonjose <91620722+Davidsonjose@users.noreply.github.com>
2023-11-13 14:17:26 +0100

    [update]-added broadcast to entities

--- src/resources/broadcast/broadcast.module.ts
+++ src/resources/broadcast/broadcast.module.ts
@@ -3,10 +3,12 @@ import { BroadcastService } from './broadcast.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BroadcastMessage } from './broadcast.entity';
 import { User } from '../auth/user.entity';
+import { BroadcastController } from './broadcast.controller';
 
 @Module({
   providers: [BroadcastService],
   imports: [TypeOrmModule.forFeature([BroadcastMessage, User])],
   exports: [BroadcastService],
+  controllers: [BroadcastController],
 })
 export class BroadcastModule {}
